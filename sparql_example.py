9158aa8f505ad9d095198b03eb24eb0af52bba59
Pablo <pablo.riutort@gmail.com>
2019-03-13 16:23:01 +0100

    Add SPARQL queries for ASK and CONSTRUCT
    Also, modify SELECT to use namespaces

--- sparql_example.py
+++ sparql_example.py
@@ -1,14 +1,47 @@
-from brett import big_brain, namespaces as ns
+from brett import big_brain
+from rdflib.namespace import FOAF, RDF
+from rdflib.plugins.sparql import prepareQuery
 
 
+namespaces = {
+    'foaf': FOAF,
+    'rdf': RDF,
+    'brett': 'http://www.semanticweb.org/pablo/ontologies/2019/brett#'
+}
 
-def works_for():
-    works_for_query = """
-        SELECT ?person_name ?another_person_name WHERE{
-            ?person <prefix>works_for ?another_person
-            ?person foaf:name ?person_name .
-            ?person foaf:name ?another_person_name .
-        }
+ask_query = prepareQuery(
     """
-    for row in big_brain.query(works_for_query):
-        print("%s works for %s" % row)
+    ASK WHERE{
+        ?person brett:works_for ?another_person .
+        ?another_person rdf:type brett:bitch .
+    }
+    """, initNs=namespaces)
+
+for result in big_brain.query(ask_query):
+    print(result)
+
+
+works_for_query = prepareQuery("""
+
+    SELECT ?person ?another_person_name 
+    WHERE{
+        ?person brett:works_for ?another_person .
+        ?another_person foaf:name ?another_person_name .
+    }
+    """, initNs=namespaces)
+
+for row in big_brain.query(works_for_query):
+    print("%s works for %s" % row)
+
+
+construct_query = prepareQuery("""
+    CONSTRUCT 
+        { ?person foaf:name "Jules Winnfield" . } 
+    WHERE{
+        ?person brett:works_for ?another_person;
+                rdf:type brett:black .
+    }
+    """, initNs=namespaces)
+
+for row in big_brain.query(construct_query):
+    print(row)
